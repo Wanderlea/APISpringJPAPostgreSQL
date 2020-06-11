5076203230bff4f875719cb7e06478ccb20550aa
Wanderlea Lodi <wgnoato@gmail.com>
2020-06-11 12:15:28 -0300

    src/main/java/spring/api/rest/controller/IndexController.java

--- src/main/java/spring/api/rest/controller/IndexController.java
+++ src/main/java/spring/api/rest/controller/IndexController.java
@@ -1,11 +1,16 @@
 package spring.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import spring.api.rest.model.Users;
 
 //annotation is used to create RESTful web services 
 @RestController
@@ -14,9 +19,25 @@ public class IndexController {
 	
 	/* Restfull service */
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init(@RequestParam(value = "nome", required = false) String nome) {
-		System.out.println("Paramentro: "+ nome);
-		return new ResponseEntity("Hello User : "+ nome, HttpStatus.OK);
+	public ResponseEntity<Users> init() {
+		
+		Users user = new Users();
+		user.setId(1L);
+		user.setLogin("Admin");
+		user.setName("Administrator");
+		user.setPassword("123");
+		
+		Users user1 = new Users();
+		user1.setId(2L);
+		user1.setLogin("Admin1");
+		user1.setName("Administrator1");
+		user1.setPassword("1234");
+		
+		List<Users> users = new ArrayList<Users>();
+		users.add(user);
+		users.add(user1);
+
+		return new ResponseEntity(users, HttpStatus.OK);
 	}
 
 }
